a235358fcd395a1b57b9446907816f1e9e2bab10
leilvlong <1455013965@qq.com>
2019-01-20 17:28:36 +0800

    hello java

---
+++ Doobee.java
@@ -0,0 +1,14 @@
+// javaÑ§Ï°£º Êä³ödoobeedoobeedo
+public class Doobee{
+    public static void main( String args[]){
+        int x = 0;
+        while( x<2 ){
+            System.out.print("doo");
+            System.out.print("bee");
+            if ( x==1 ){
+                System.out.print("do");
+            }
+            x++;
+        }
+    }
+}
